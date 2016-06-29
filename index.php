fbcf79ed4edc6cae67f56390760097a76b676617
Pavlinus <pavlin.kov@mail.ru>
2016-06-29 10:10:40 +0300

    Not adaptive landing

---
+++ index.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="ru-ru" dir="ltr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+<meta name="description" content="..." />
+<meta name="keywords" content="..." />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<title>Landing One</title>
+<!--[if IE]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body id="page">
+  <section id="header">
+    <div class="layout">
+      <img src="images/logo.png"/>
+      <menu>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Pricing Table</a></li>
+        <li><a href="#">How It Work</a></li>
+        <li><a href="#">Happy Clients</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </menu>
+      <div class="clr"></div>
+    </div>
+
+    <div class="layout">
+      <div class="headerMiddle">
+        <h2>Boost up your local business</h2>
+        <span class="headline">Some text goes here</span>
+
+        <div class="layout">
+          <!-- Форма регистрации -->
+          <div class="requestForm">
+            <form>
+              <div class="inputBox">
+                <img src="images/name_icon.png"/>
+                <input type="name" name="formName" placeholder="Full Name"/>
+              </div>
+              <div class="inputBox">
+                <img src="images/email_icon.png"/>
+                <input type="email" name="formEmail" placeholder="Email Address"/>
+              </div>
+              <div class="inputBox">
+                <img src="images/phone_icon.png"/>
+                <input type="phone" name="formPhone" placeholder="Phone Number"/>
+              </div>
+              <input type="submit" value="Request a quote" class="button"/>
+            </form>
+            <div class="clr"></div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slider">
+        <img src="images/laptop.png"/>
+        <a href="#" id="slide1"><img src="images/dot.png"/></a>
+        <a href="#" id="slide2"><img src="images/dot.png"/></a>
+        <a href="#" id="slide3"><img src="images/dot.png"/></a>
+      </div>
+    </div>
+  </section>
+
+  <section id="services">
+    <div class="layout">
+      <h2>Our services</h2>
+      <span class="headline">Headline text</span>
+
+      <div class="box2">
+        <img src="images/font_icons.png"/>
+        <div>
+          <h4>Font-Awesome Icons</h4>
+          <p>Some text goes here Some text goes here Some text goes here
+          Some text goes here Some text goes here Some text goes here</p>
+        </div>
+      </div>
+      <div class="box2">
+        <img src="images/responsive_design.png"/>
+        <div>
+          <h4>Fully responsive Design</h4>
+          <p>Some text goes here Some text goes here Some text goes here
+          Some text goes here Some text goes here Some text goes here</p>
+        </div>
+      </div>
+
+      <div class="box2">
+        <img src="images/working_form.png"/>
+        <div>
+          <h4>Working Contract Form</h4>
+          <p>Some text goes here Some text goes here Some text goes here
+          Some text goes here Some text goes here Some text goes here</p>
+        </div>
+      </div>
+      <div class="box2">
+        <img src="images/retina.png"/>
+        <div>
+          <h4>Retina ready icons here</h4>
+          <p>Some text goes here Some text goes here Some text goes here
+          Some text goes here Some text goes here Some text goes here</p>
+        </div>
+      </div>
+
+      <div class="box2 last_box2">
+        <img src="images/friendly_code.png"/>
+        <div>
+          <h4>Friendly code</h4>
+          <p>Some text goes here Some text goes here Some text goes here
+            Some text goes here Some text goes here Some text goes here</p>
+        </div>
+      </div>
+      <div class="box2 last_box2">
+        <img src="images/flat_design.png"/>
+        <div>
+          <h4>Flat Trendy landing page design</h4>
+          <p>Some text goes here Some text goes here Some text goes here
+            Some text goes here Some text goes here Some text goes here</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="about">
+    <div class="layout">
+      <img src="images/about_us.jpg"/>
+      <div>
+        <h2>About us</h2>
+        <p>Some text goes here Some text goes here Some text goes here
+          Some text goes here Some text goes here Some text goes here
+          Some text goes here Some text goes here Some text goes here
+            Some text goes here Some text goes here Some text goes here
+            Some text goes here Some text goes here Some text goes here
+              Some text goes here Some text goes here Some text goes here</p>
+        <a href="#">Read more</a>
+    </div>
+    </div>
+  </section>
+
+  <section id="price">
+    <div class="layout">
+      <h2>Choose your price</h2>
+      <span class="headline">Headline text</span>
+
+      <div class="box3">
+        <h2>Basic plan</h2>
+        <span class="price_sign">&#36;</span>
+        <span class="price_digit">22</span>
+        <p class="price_details">Per month</p>
+        <div class="plan_option">
+          <p>1 Account</p>
+        </div>
+        <div class="plan_option">
+          <p>8 Hour Support</p>
+        </div>
+        <div class="plan_option">
+          <p>Custom cloud services</p>
+        </div>
+        <a href="#">Get started</a>
+      </div>
+
+      <div class="box3 premium">
+        <h2>Premium plan</h2>
+        <span class="price_sign">&#36;</span>
+        <span class="price_digit">42</span>
+        <p class="price_details">Per month</p>
+        <div class="plan_option">
+          <p>2 Account</p>
+        </div>
+        <div class="plan_option">
+          <p>100Mb storage</p>
+        </div>
+        <div class="plan_option">
+          <p>Custom cloud services</p>
+        </div>
+        <a href="#">Get started</a>
+      </div>
+
+      <div class="box3">
+        <h2>Gold plan</h2>
+        <span class="price_sign">&#36;</span>
+        <span class="price_digit">62</span>
+        <p class="price_details">Per month</p>
+        <div class="plan_option">
+          <p>8 Account</p>
+        </div>
+        <div class="plan_option">
+          <p>24 Hour Support</p>
+        </div>
+        <div class="plan_option">
+          <p>Custom cloud services</p>
+        </div>
+        <a href="#">Get started</a>
+      </div>
+    </div>
+  </section>
+
+  <section id="work">
+    <div class="layout">
+      <h2>How it works</h2>
+      <span class="headline">Headline text</span>
+
+      <div class="box2">
+        <img src="images/double_arrow.png"/>
+      </div>
+      <div class="box2">
+        <img src="images/double_arrow_g.png"/>
+      </div>
+      <div class="clr"></div>
+
+      <div class="box4">
+        <img src="images/step1.png"/>
+      </div>
+      <div class="box4">
+        <img src="images/step2.png"/>
+      </div>
+      <div class="box4">
+        <img src="images/step3.png"/>
+      </div>
+      <div class="box4">
+        <img src="images/step4.png"/>
+      </div>
+      <div class="clr"></div>
+
+      <div class="box">
+        <img src="images/double_arrow_g_rotate.png"/>
+      </div>
+
+      <div class="box4">
+        <h4>Step 1</h4>
+        <p>Step 1 description</p>
+      </div>
+      <div class="box4">
+        <h4>Step 2</h4>
+        <p>Step 2 description</p>
+      </div>
+      <div class="box4">
+        <h4>Step 3</h4>
+        <p>Step 3 description</p>
+      </div>
+      <div class="box4">
+        <h4>Step 4</h4>
+        <p>Step 4 description</p>
+      </div>
+      <div class="clr"></div>
+      <a href="#">Get start now</a>
+    </div>
+  </section>
+
+  <section id="clients">
+    <div class="layout">
+      <h2>Happy clients</h2>
+      <span class="headline">Headline text</span>
+
+        <div>
+        <div class="box2">
+          <img src="images/client1.jpg"/>
+          <div>
+            <p>Client's response Client's response Client's response
+            Client's response Client's response</p>
+            <span class="response_quots">&#8222;</span>
+            <span class="client_name">Darwin Michel,</span>
+            <span class="client_rank">Project manager</span>
+          </div>
+        </div>
+        <div class="box2">
+          <img src="images/client2.jpg"/>
+          <div>
+            <p>Client's response Client's response Client's response
+            Client's response Client's response Client's</p>
+            <span class="response_quots">&#8222;</span>
+            <span class="client_name">Adam Sabath,</span>
+            <span class="client_rank">Creative director</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="box2">
+        <img src="images/client3.jpg"/>
+        <div>
+          <p>Client's response Client's response Client's response
+          Client's response Client's response Client's</p>
+          <span class="response_quots">&#8222;</span>
+          <span class="client_name">Bob Carter,</span>
+          <span class="client_rank">Lipsum Director</span>
+        </div>
+      </div>
+      <div class="box2">
+        <img src="images/client4.jpg"/>
+        <div>
+          <p>Client's response Client's response Client's response
+          Client's response Client's response Client's</p>
+          <span class="response_quots">&#8222;</span>
+          <span class="client_name">Zack Stark,</span>
+          <span class="client_rank">Manager</span>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contacts">
+    <div class="layout">
+      <h2>Newsletter</h2>
+      <span class="headline">Headline text</span>
+
+      <!-- Форма подписки -->
+      <div class="requestForm">
+        <form>
+          <div class="inputBox">
+            <img src="images/name_icon.png"/>
+            <input type="name" name="formName" placeholder="Full Name"/>
+          </div>
+          <div class="inputBox">
+            <img src="images/email_icon.png"/>
+            <input type="email" name="formEmail" placeholder="Email Address"/>
+          </div>
+          <input type="submit" value="Subscribe now" class="button"/>
+        </form>
+      </div>
+      <div class="clr"></div>
+
+      <div class="contacts_map">
+        <div class="contacts_circle">
+          <h4>Address</h4>
+          <p>Business street</p>
+          <p>Second north section</p>
+          <p>Bavani nagar. 88769</p>
+          <p>Call : +987-9976-999</p>
+          <span class="email_contact">info@businessface.com</span>
+        </div>
+        <img src="images/logo.png"/>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="layout">
+      <menu>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Pricing Table</a></li>
+        <li><a href="#">How It Work</a></li>
+        <li><a href="#">Happy Clients</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </menu>
+      <div class="clr"></div>
+
+      <div class="contact_number">
+        <img src="images/footer_phone.png"/>
+        <span class="number">+987&nbsp;9976&nbsp;999</span>
+      </div>
+
+      <div class="social">
+        <a href="#" class="google"></a>
+        <a href="#" class="in"></a>
+        <a href="#" class="twitter"></a>
+        <a href="#" class="facebook"></a>
+      </div>
+    </div>
+
+    <div class="layout">
+      <div class="copyright">
+        <p>All contents &copy; 2014 Business Theme.
+          All rights reserved Designed by : akhilwebfolio</p>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
